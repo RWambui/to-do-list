42e8aca23688fd2c72be23e8b8dab0470c95bc82
Rose <rosemwangi305@gmail.com>
2022-05-31 22:57:39 +0300

    adding delete functionality

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
 
 const TodoList = ({todos, setTodos}) => {
+
+  const handleDelete = ({id}) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
   return (
     <div>
-      {todos.map((todos) => (
+      {todos.map((todo) => (
         <li 
         className="list-item" 
-        key={todos.id}
+        key={todo.id}
         >
-
           <input 
           type="text" 
-          value={todos.title} 
+          value={todo.title} 
           className='list' 
           onChange={(e) => e.preventDefault}
           />
-
+          <button className="button-complete task-button">
+            <i className="fa fa-check-circle"></i>
+          </button>
+          <button className="button-edit task-button">
+            <i className="fa fa-edit"></i>
+          </button>
+          <button className="button-delete task-button" onClick={() => handleDelete(todo)}>
+            <i className="fa fa-trash"></i>
+          </button>
         </li>
       ))}
     </div>
